d561e17c13dc64a99ea8c1cf03adeed94572c38c
Rohigithubt <rohitjain68134@gmail.com>
2023-08-02 10:04:23 +0530

    reverse array in c language

--- reverse_an_array_in_c.c
+++ reverse_an_array_in_c.c
@@ -1,38 +1,3 @@
-//program for reverse an array in c++
-#include<bits/stdc++.h>
-using namespace std;
-void reverse(int arr[],int size){     //reverse function
-    int temp,start=0,end=size-1;
-    while(start<end){
-        temp=arr[start];     //swapping process
-        arr[start]=arr[end];
-        arr[end]=temp;
-        start++;
-        end--;
-    }
-}
-int main()
-{
-    int size;
-    cout<<"Enter the size of an array : ";
-    cin>>size;
-    int arr[size];
-    cout<<"Enter the elements in an array : "<<endl;
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
-    }
-    cout<<"Original Array : "<<endl;
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<"\t";
-    }
-    cout<<endl;
-    reverse(arr,size);  //function call
-    cout<<"Array after reverse operation : "<<endl;
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<"\t";
-    }
-}
-
 //program for reverse an array in c
 #include<stdio.h>
 #include<conio.h>
@@ -71,15 +36,13 @@ int main()
     return(0);
 }
 
-/* output  ->
-Enter the size of an array : 5
-Enter the elements in an array : 
-1
-2
-3
-4
+
+/*  output->
+Enter the size of an array : 
 5
-Original Array : 
+Enter the elements in an array : 
+1 2 3 4 5
+Original array : 
 1       2       3       4       5
 Array after reverse operation : 
 5       4       3       2       1
