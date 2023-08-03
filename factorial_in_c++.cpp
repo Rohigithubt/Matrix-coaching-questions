1e508dd425aa944c341bc900a69e68309549dcfa
Rohigithubt <rohitjain68134@gmail.com>
2023-08-03 12:57:22 +0530

    modified code

--- factorial_in_c++.cpp
+++ factorial_in_c++.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int factorial(int n){
     if(n==0 || n==1){
-        return n;
+        return 1;
     }
     return n*factorial(n-1);    //recursion call
 }
--- factorial_in_c.c
+++ factorial_in_c.c
@@ -3,7 +3,7 @@
 #include<conio.h>
 int factorial(int n){
     if(n==0 || n==1){
-        return n;
+        return 1;
     }
     return n*factorial(n-1); //recursion call
 }
