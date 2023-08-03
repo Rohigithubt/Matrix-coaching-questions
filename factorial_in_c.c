6cd885bd7a83d59858c2c825dbc389fd7402672e
Rohigithubt <rohitjain68134@gmail.com>
2023-08-03 10:48:23 +0530

    factorial program in c language

---
+++ factorial_in_c.c
@@ -0,0 +1,21 @@
+//factorial program in c
+#include<stdio.h>
+#include<conio.h>
+int factorial(int n){
+    if(n==0 || n==1){
+        return n;
+    }
+    return n*factorial(n-1); //recursion call
+}
+int main()
+{
+    int n;
+    printf("Enter the number you want to find factorial of :\n");
+    scanf("%d",&n);
+    printf("The factorial of %d is=%d",n,factorial(n));   //function call
+}
+/* output->
+Enter the number you want to find factorial of :
+5
+The factorial of 5 is=120
+*/
